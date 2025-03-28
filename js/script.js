3c1cbd03684bd86fb73e1d54f489b871ed68650d
Zoksss <zorantodorovic80@gmail.com>
2025-03-28 21:07:48 +0100

    transitions

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ let headerIndexNum = document.querySelectorAll(".heading-track_item-num");
 let headingText = document.querySelectorAll(".heading-text");
 
 
-let textStyle = "transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); opacity: 1; transform-style: preserve-3d;"
 let currentId = 1; // 1,2,3
+
+
+let headingProjectIds = [0, 4, 3] // id from projects.js array 0-7
 
 const updateHeaderIndexNum = () => {
     headerIndexNum.forEach(headerIndexNumEl => {
@@ -16,13 +18,40 @@ const updateHeaderIndexNum = () => {
 }
 
 const updateText = () => {
-    letNewText = ["Seaside", `<span class="text-span">Horizon</span>`, "Vila"];
-    for (let i = 0; i <= 3; i++) {
-        headingText[i].style = "";
-        if (headingText[i].classList.contains("text-anim")) headingText[i].classList.remove("text-anim");
-        headingText[i].classList.add(`text-anim${i+1}`);
-        headingText[i].innerHTML = letNewText[i];
+    let newText = projectsJson[headingProjectIds[currentId-1]]["Name"].split(" ");
+    let textSpan = `<span class="text-span">${newText[1]}</span>`;
+    newText[1] = textSpan;
 
+    // Promena teksta
+    for (let i = 0; i < 3; i++) {
+        if (headingText[i]) {
+            console.log(headingText[i]);
+            // Uklanjanje animacije kako bi je ponovo pokrenuli
+            headingText[i].classList.remove(`text-anim${i+1}`);
+
+            // Moramo da koristimo "reflow" da bi animacija ponovo počela
+            void headingText[i].offsetWidth; // Ovo je trik za reflow (resetovanje stila)
+
+            // Dodavanje nove animacije
+            headingText[i].classList.add(`text-anim${i+1}`);
+
+            // Promena sadržaja
+            headingText[i].innerHTML = newText[i];
+        } else {
+            console.log(`Element na poziciji ${i} nije pronađen!`);
+        }
+    }
+
+    // Promena pozadinske slike
+    const headerWrapper = document.querySelector('.header-wrapper');
+    const projectImage = projectsJson[headingProjectIds[currentId-1]]["CoverImage"];  // Pretpostavljam da u JSON-u imaš URL slike
+
+    if (headerWrapper && projectImage) {
+        // Dodajemo klasu za animaciju pozadinske slike
+        headerWrapper.classList.add('image-change-animation'); 
+
+        // Menjamo pozadinsku sliku
+        headerWrapper.style.backgroundImage = `url(${projectImage})`;
     }
 }
 
@@ -37,5 +66,11 @@ headerPrev.addEventListener("click", () => {
     if (currentId <= 1) currentId = 3;
     else currentId--;
     updateHeaderIndexNum();
+    updateText();
 })
 
+const setId = (id) => {
+    currentId = id;
+    updateHeaderIndexNum();
+    updateText();
+}
