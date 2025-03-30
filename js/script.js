c6ea1e0d99dab7f2bdb2e6594b64bb84e6a113f5
Zoksss <zorantodorovic80@gmail.com>
2025-03-30 15:01:42 +0200

    imgs and counter but need fix

--- js/script.js
+++ js/script.js
@@ -5,10 +5,18 @@ let headerIndexNum = document.querySelectorAll(".heading-track_item-num");
 let headingText = document.querySelectorAll(".heading-text");
 
 
+let countersContainer = document.querySelector(".stats")
+let counters = document.querySelectorAll(".stats-number")
+
+let counterActivated = false;
+let counterTargets = [20, 7, 100];
+
+
 let currentId = 1; // 1,2,3
 
 
 let headingProjectIds = [0, 4, 3] // id from projects.js array 0-7
+
 
 const updateHeaderIndexNum = () => {
     headerIndexNum.forEach(headerIndexNumEl => {
@@ -18,7 +26,7 @@ const updateHeaderIndexNum = () => {
 }
 
 const updateText = () => {
-    let newText = projectsJson[headingProjectIds[currentId-1]]["Name"].split(" ");
+    let newText = projectsJson[headingProjectIds[currentId - 1]]["Name"].split(" ");
     let textSpan = `<span class="text-span">${newText[1]}</span>`;
     newText[1] = textSpan;
 
@@ -27,13 +35,13 @@ const updateText = () => {
         if (headingText[i]) {
             console.log(headingText[i]);
             // Uklanjanje animacije kako bi je ponovo pokrenuli
-            headingText[i].classList.remove(`text-anim${i+1}`);
+            headingText[i].classList.remove(`text-anim${i + 1}`);
 
             // Moramo da koristimo "reflow" da bi animacija ponovo počela
             void headingText[i].offsetWidth; // Ovo je trik za reflow (resetovanje stila)
 
             // Dodavanje nove animacije
-            headingText[i].classList.add(`text-anim${i+1}`);
+            headingText[i].classList.add(`text-anim${i + 1}`);
 
             // Promena sadržaja
             headingText[i].innerHTML = newText[i];
@@ -44,11 +52,11 @@ const updateText = () => {
 
     // Promena pozadinske slike
     const headerWrapper = document.querySelector('.header-wrapper');
-    const projectImage = projectsJson[headingProjectIds[currentId-1]]["CoverImage"];  // Pretpostavljam da u JSON-u imaš URL slike
+    const projectImage = projectsJson[headingProjectIds[currentId - 1]]["CoverImage"];  // Pretpostavljam da u JSON-u imaš URL slike
 
     if (headerWrapper && projectImage) {
         // Dodajemo klasu za animaciju pozadinske slike
-        headerWrapper.classList.add('image-change-animation'); 
+        headerWrapper.classList.add('image-change-animation');
 
         // Menjamo pozadinsku sliku
         headerWrapper.style.backgroundImage = `url(${projectImage})`;
@@ -74,3 +82,29 @@ const setId = (id) => {
     updateHeaderIndexNum();
     updateText();
 }
+
+
+
+window.addEventListener("scroll", () => {
+    if (pageYOffset > countersContainer.offsetTop - countersContainer.offsetHeight - 500 && !counterActivated) {
+        counters.forEach(counter => {
+            //counter.innerText = 0;
+            let count = 0;
+            let target = parseInt(counter.innerText);
+            const updateCount = () => {
+                console.log("target: " + target)
+
+                if (count < target) {
+                    count++;
+                    counter.innerText = count;
+                    setTimeout(updateCount, counter>25?125:55);
+                } else {
+                    count.innerText = target;
+                }
+            }
+            updateCount();
+            counterActivated = true;
+
+        })
+    }
+})
