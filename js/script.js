ae585b6bd033ed1f290bd830d0b83cd52f285a90
Zoksss <zorantodorovic80@gmail.com>
2025-03-31 17:12:15 +0200

    filter and fixes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 let headerPrev = document.querySelector("#headerPrev");
 let headerNext = document.querySelector("#headerNext");
 
+
+let filterAll = document.querySelector("#filterAll");
+let filterBuilding = document.querySelector("#filterBuilding");
+let filterInterior = document.querySelector("#filterInterior");
+let projectsWrapper = document.querySelector(".latest-project-wrapper");
+
 let headerIndexNum = document.querySelectorAll(".heading-track_item-num");
 let headingText = document.querySelectorAll(".heading-text");
 
 
 let countersContainer = document.querySelector(".stats")
 let counters = document.querySelectorAll(".stats-number")
+
+
 
 let counterActivated = false;
 let counterTargets = [20, 7, 100];
@@ -50,17 +58,6 @@ const updateText = () => {
         }
     }
 
-    // Promena pozadinske slike
-    const headerWrapper = document.querySelector('.header-wrapper');
-    const projectImage = projectsJson[headingProjectIds[currentId - 1]]["CoverImage"];  // Pretpostavljam da u JSON-u imaš URL slike
-
-    if (headerWrapper && projectImage) {
-        // Dodajemo klasu za animaciju pozadinske slike
-        headerWrapper.classList.add('image-change-animation');
-
-        // Menjamo pozadinsku sliku
-        headerWrapper.style.backgroundImage = `url(${projectImage})`;
-    }
 }
 
 headerNext.addEventListener("click", () => {
@@ -92,29 +89,29 @@ window.addEventListener("scroll", () => {
 
         function updateCount12() {
             count12Counter++;
-        
+
             if (count12Counter <= counterTargets[0]) {
                 counters[0].innerText = count12Counter;
             } else {
                 counters[0].innerText = counterTargets[0];
             }
-        
+
             if (count12Counter < counterTargets[1]) {
                 counters[1].innerText = count12Counter;
             } else {
                 counters[1].innerText = counterTargets[1];
             }
-            
-            if(count12Counter > 10) counter12Delay = 130;
-            if(count12Counter > 16) counter12Delay = 270;
-            
+
+            if (count12Counter > 10) counter12Delay = 130;
+            if (count12Counter > 16) counter12Delay = 270;
+
 
             // Nastavlja se sa setTimeout samo ako brojač nije dostigao ciljeve
             if (count12Counter <= counterTargets[0] || count12Counter < counterTargets[1]) {
                 setTimeout(updateCount12, counter12Delay); // Rekurzivni poziv sa delay-em od 100ms
             }
         }
-        
+
         // Početni poziv za count12Counter
         setTimeout(updateCount12, counter12Delay);
 
@@ -149,3 +146,65 @@ window.addEventListener("scroll", () => {
 
     }
 })
+
+
+const updateProjectFilters = (filter) => {
+    console.log(filter);
+
+    // Dodavanje ili uklanjanje aktivnih klasa na osnovu filtera
+    if (filter === "all") filterAll.classList.add("link-active");
+    else filterAll.classList.remove("link-active");
+
+    if (filter === "building") filterBuilding.classList.add("link-active");
+    else filterBuilding.classList.remove("link-active");
+
+    if (filter === "interior") filterInterior.classList.add("link-active");
+    else filterInterior.classList.remove("link-active");
+
+    // Filtriranje projekata na osnovu filtera
+    let filteredProjects = [];
+    if (filter === "all") {
+        // Ako je filter "all", uzimamo sve projekte
+        filteredProjects = projectsJson;
+    } else {
+        // Filtriramo projekte na osnovu filterId
+        filteredProjects = projectsJson.filter(project => project.filterId === filter);
+    }
+
+    // Prikazivanje filtriranih projekata (možete ovde dodati logiku za prikazivanje)
+    console.log(filteredProjects);
+
+    projectsWrapper.innerHTML = "";
+
+    // Kreiraj HTML za projekte
+    let projectDom = "";
+    for (let i = 0; i < 4; i++) {
+        projectDom += ` 
+            <div role="listitem" class="latest-project-item w-dyn-item">
+                <a href="/larson/projects/${filteredProjects[i].Slug}" class="latest-project-item w-inline-block">
+                    <img src="${filteredProjects[i].Thumbnail}" loading="lazy" alt="" class="latest-project-item-image">
+                    <p class="latest-project-item-name">${filteredProjects[i].Name}</p>
+                    <div class="latest-project-hover">
+                        <h3 class="latest-project-hover-title">${filteredProjects[i].Name}</h3>
+                        <p class="latest-project-hover-desc">${filteredProjects[i].Service1Desc}</p>
+                        <h3 class="latest-project-hover-see-project">See Project &gt;</h3>
+                    </div>
+                </a>
+            </div>
+        `;
+    }
+    projectsWrapper.innerHTML = projectDom;
+
+    // GSAP stagger animacija
+    const projectItems = document.querySelectorAll('.latest-project-item');
+    gsap.from(projectItems, {
+        opacity: 0,
+        y: 20,  // pomeraj sa donje strane
+        stagger: 0.1,  // vremenski interval između animacija
+        duration: 0.7  // trajanje animacije
+    });
+};
+
+filterAll.addEventListener("click", () => updateProjectFilters("all"));
+filterBuilding.addEventListener("click", () => updateProjectFilters("building"));
+filterInterior.addEventListener("click", () => updateProjectFilters("interior"));
